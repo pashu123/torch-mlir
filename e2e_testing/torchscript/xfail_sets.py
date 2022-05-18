8e473189ddb3dab83511efdaeba469abff900720
Gaurav Shukla <gaurav@nod-labs.com>
2022-05-18 21:59:04 +0530

    [TORCH] Add decomposition of `aten.linear` op
    This commit adds decomposition of `aten.linear` op.
    
    Signed-Off-by: Gaurav Shukla

--- e2e_testing/torchscript/xfail_sets.py
+++ e2e_testing/torchscript/xfail_sets.py
@@ -93,6 +93,7 @@ TOSA_PASS_SET = {
     "ElementwiseReciprocalModule_basic",
     "TypePromotionAlphaWiderModule_basic",
     "Conv2dWithPaddingDilationStrideStaticModule_basic",
+    "BatchMlpLayerModule_basic",
     "BatchNorm1DModule_basic",
     "BatchNorm1DWith2DInputModule_basic",
     "BatchNorm2DModule_basic",
